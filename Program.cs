a693ce075636e70c769676bdd45dcc4abb93b6f9
Fabio-Tecno <fmbinho8@gmail.com>
2025-11-08 23:58:22 -0300

    Projeto iniciado

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using projeto_minimal_api.Dominio.DTOs;
+using projeto_minimal_api.Infraestrutura;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+  options.UseMySql(
+     builder.Configuration.GetConnectionString("mysql"),
+     ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+
+  );
+});
+
+var app = builder.Build();
+
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/login", (LoginDTO loginDTO) => {
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+        return Results.Ok("Login com sucesso");
+    else
+        return Results.Unauthorized();
+});
+
+
+app.Run();
+
